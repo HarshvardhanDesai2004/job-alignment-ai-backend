6461d0c19eaea47b38d94cc65a454664b49629c0
Harshvardhan Desai <harshvardhandesai2004@gmail.com>
2026-03-16 00:27:17 +0530

    trust on proxy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+app.set("trust proxy", 1);
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const resumeRoutes = require("./routes/resumeRoutes");
